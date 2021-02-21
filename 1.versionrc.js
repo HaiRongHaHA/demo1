98edec62493dd79476dfe4b37e81c944804171db
王海容 <wanghairong@weimiao.cn>
2021-02-21 21:23:04 +0800

    :bug: feat: dsfd

---
+++ 1.versionrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "types": [
+    { "type": "feat", "section": "✨Features新特性" },
+    { "type": "fix", "section": "🐛Bug Fixes修复了个bug" },
+    { "type": "🎉init", "section": "🎉Init初始化" },
+    { "type": "✏️docs", "section": "✏️Docs文档" },
+    { "type": "💄style", "section": "💄Style格式化" },
+    { "type": "♻️refactor", "section": "♻️Refactor重构" },
+    { "type": "⚡️perf", "section": "⚡️Perf性能优化" },
+    { "type": "✅test", "hidden": true },
+    { "type": "⏪revert", "hidden": true },
+    { "type": "📦build", "hidden": true },
+    { "type": "🚀chore", "hidden": true },
+    { "type": "👷ci", "hidden": true }
+  ]
+}
