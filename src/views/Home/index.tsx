b59dc3c8258cc275f6c59684f430d5737f8e8192
HaiRongHaHA <whr9743@163.com>
2021-05-23 11:39:52 +0800

    :sparkles: feat(components): 首页背景样式&装饰样式完成

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,9 +1,18 @@
 import { defineComponent } from 'vue';
+import classes from './index.module.scss';
 
 const Home = defineComponent({
   setup() {
     return () => (
-      <div class>home</div>
+      <div class={classes.home}>
+        <div class={classes.home__moon}></div>
+        <div class={classes['home__star-gruop']}>
+          <div class={classes.home__star}></div>
+          <div class={classes.home__star}></div>
+          <div class={classes.home__star}></div>
+          <div class={classes.home__star}></div>
+        </div>
+      </div>
     );
   },
 });
